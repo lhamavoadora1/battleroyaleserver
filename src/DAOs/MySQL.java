ed723767cc35c273a8bde0063d46255ff20ebcc7
iagomes <54648438+iagomes@users.noreply.github.com>
2020-06-16 22:40:53 -0300

    Iago
    Inclusão dos inserts ao banco

--- src/DAOs/MySQL.java
+++ src/DAOs/MySQL.java
@@ -1,6 +1,8 @@
 package DAOs;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -67,43 +69,132 @@ public class MySQL implements DAO {
 		
 	}
 	
-	Boolean insert(Arena arena) {
-		String name = arena.getName();
-		String grid = jsonParser.toJson(arena.getGrid());
-		// TODO
-		return true;
+	void insert(Arena arena) throws SQLException {
+		String name  = arena.getName();
+		String grid  = jsonParser.toJson(arena.getGrid());
+		String query = "INSERT INTO ARENA (ARN_NAME, ARN_GRID) VALUES (?, ?);";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, grid);
+			
+			stmt.execute();			
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		
 	}
-	Boolean insert(Ground ground) {
+	void insert(Ground ground) throws SQLException {
 		String name     = ground.getName();
 		String type     = ground.getType();
 		String imageUrl = ground.getImageUrl();
-		// TODO
-		return true;
+		String query 	= "INSERT INTO GROUND (GRD_NAME, GRD_TYPE, GRD_IMAGE_URL) VALUES (?, ?, ?);";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			
+			stmt.execute();			
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+		
 	}
-	Boolean insert(Wall wall) {
+	void insert(Wall wall) throws SQLException {
 		String name     = wall.getName();
 		String type     = wall.getType();
 		String imageUrl = wall.getImageUrl();
-		// TODO
-		return true;
+		String query 	=  "INSERT INTO WALL (WLL_NAME, WLL_TYPE, WLL_IMAGE_URL) VALUES (?, ?, ?);";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			
+			stmt.execute();
+			connection.close();			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
 	}
-	Boolean insert(Weapon weapon) {
+	void insert(Weapon weapon) throws SQLException {
 		String name     = weapon.getName();
 		String type     = weapon.getType();
 		String imageUrl = weapon.getImageUrl();
 		Integer damage  = weapon.getDamage();
 		String distance = jsonParser.toJson(weapon.getImageUrl());
-		// TODO
-		return true;
+		String query 	= "INSERT INTO WEAPON (WPN_NAME, WPN_TYPE, WPN_IMAGE_URL, WPN_DAMAGE, WPN_DISTANCE) VALUES (?, ?, ?, ?, ?);";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			stmt.setInt(4, damage);
+			stmt.setString(5, distance);
+			
+			stmt.execute();
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+
 	}
-	Boolean insert(Player player) {
+	
+	void insert(Player player) throws SQLException {
 		String id         = player.getId();
 		String name       = player.getName();
-		String type       = player.getType();
 		String imageUrl   = player.getImageUrl();
 		String weaponName = player.getWeapon().getName();
-		// TODO
-		return true;
+		String query = "INSERT INTO PLAYER (PLR_ID, PLR_NAME, PLR_IMAGE_URL, FK_WPN_NAME) VALUES (?, ?, ?, ?);";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, id);
+			stmt.setString(2, name);
+			stmt.setString(3, imageUrl);
+			stmt.setString(4, weaponName);
+			
+			stmt.execute();
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
 	}
 
 	@Override
@@ -132,26 +223,136 @@ public class MySQL implements DAO {
 		
 	}
 	
-	Boolean update(Arena arena) {
-		// TODO
-		return true;
-	}
-	Boolean update(Ground ground) {
-		// TODO
-		return true;
-	}
-	Boolean update(Wall wall) {
-		// TODO
-		return true;
-	}
-	Boolean update(Weapon weapon) {
-		// TODO
-		return true;
-	}
-	Boolean update(Player player) {
-		// TODO
-		return true;
-	}
+	void update(Arena arena) throws SQLException {
+/*		String name  = arena.getName();
+		String grid  = jsonParser.toJson(arena.getGrid());
+		String query = "UPDATE ARENA SET ARN_NAME = ?, ARN_GRID = ? WHERE ARN_NAME = '?';";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, grid);
+			stmt.setString(3, name);
+			
+			stmt.execute();			
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+*/	}
+	void update(Ground ground) throws SQLException {
+/*		String name     = ground.getName();
+		String type     = ground.getType();
+		String imageUrl = ground.getImageUrl();
+		Integer id 		= ground.getId();
+		String query 	= "UPDATE GROUND SET GRD_NAME = ?, GRD_TYPE = ?, GRD_IMAGE_URL = ? WHERE GRD_ID = '?';";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			stmt.setInt(4, id);
+			
+			stmt.execute();			
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+*/	}
+	void update(Wall wall) throws SQLException {
+/*		String name     = wall.getName();
+		String type     = wall.getType();
+		String imageUrl = wall.getImageUrl();
+		Integer id 		= wall.getId();
+		String query 	=  "UPDATE WALL SET WLL_NAME = ?, WLL_TYPE = ?, WLL_IMAGE_URL = ? WHERE WLL_ID = ?;";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			stmt.setInt(4, id);
+			
+			stmt.execute();
+			connection.close();			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+*/	}
+	void update(Weapon weapon) throws SQLException {
+/*		String name     = weapon.getName();
+		String type     = weapon.getType();
+		String imageUrl = weapon.getImageUrl();
+		Integer damage  = weapon.getDamage();
+		String distance = jsonParser.toJson(weapon.getImageUrl());
+		Integer id		= weapon.getId();
+		String query 	= "UPDATE WEAPON SET WPN_NAME = ?, WPN_TYPE = ?, WPN_IMAGE_URL = ?, WPN_DAMAGE = ?, WPN_DISTANCE = ? WHERE WPN_ID = ?;";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, name);
+			stmt.setString(2, type);
+			stmt.setString(3, imageUrl);
+			stmt.setInt(4, damage);
+			stmt.setString(5, distance);
+			stmt.setInt(6, id);
+			
+			stmt.execute();
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+*/	}
+	void update(Player player) throws SQLException {
+/*		String id         = player.getId();
+		String name       = player.getName();
+		String type       = player.getType();
+		String imageUrl   = player.getImageUrl();
+		String weaponName = player.getWeapon().getName();
+		Integer id		  = player.getId();
+		String query 	  = "UPDATE PLAYER SET FK_WPN_ID = ? WHERE PLR_ID = ?;";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(query);
+			stmt.setString(1, weaponName);
+			stmt.setInt(2, id);
+
+			stmt.execute();
+			connection.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				connection.close();
+			}
+		}
+*/	}
 
 	@Override
 	public Boolean delete(Object obj) throws Exception {
