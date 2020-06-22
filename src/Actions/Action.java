3d5d04e1b433f59b2bea50b722db123cba0b4dea
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2020-06-22 20:13:48 -0300

    adding area damage mechanic

--- src/Actions/Action.java
+++ src/Actions/Action.java
@@ -9,83 +9,104 @@ import Things.*;
 
 public class Action extends Intention {
 	
-	public Action(Life attacker, Direction direction, Arena arena) {
+	public Action(String roundId, Life attacker, Direction direction, Arena arena) {
 		super("attack", attacker, direction, arena);
 	}
 	
 	public UI attackUI() throws Exception {
-		return attackUI(this.attacker, this.direction, this.arena);
+		if (this.isFilled()) {
+			if (this.life instanceof Player) {
+				
+				Player player = (Player)this.life;
+				
+				switch (this.direction) {
+					case UP:
+						return attack(player, Axis.Y, 1, this.arena);
+					case DOWN:
+						return attack(player, Axis.Y, -1, this.arena);
+					case LEFT:
+						return attack(player, Axis.X, -1, this.arena);
+					case RIGHT:
+						return attack(player, Axis.X, 1, this.arena);
+				}
+			}
+			
+			return null;
+		}
+		throw new Exception("Action must be instantiated first!");
+		
 	}
 	
-	public static UI attackUI(Life attacker, Direction direction, Arena arena) throws Exception {
+	UI attack(Player attacker, Axis axis, Integer frontModifier, Arena arena) throws Exception {
 		
-		if (attacker instanceof Player) {
+		Integer[][] attackDistance = attacker.getWeapon().getDistance();
+		Integer frontLength = attackDistance.length;
+		Integer sideLength = attackDistance[0].length;
+		
+		for (Integer fd = 0; fd < frontLength; fd++) {
 			
-			Player player = (Player)attacker;
+			Coordinate targetCoordinate = attacker.getPosition().get(axis, (fd + 1) * frontModifier);
 			
-			switch (direction) {
-				case UP:
-					return attack(player, Axis.Y, 1, 1, arena);
-				case DOWN:
-					return attack(player, Axis.Y, -1, 1, arena);
-				case LEFT:
-					return attack(player, Axis.X, -1, 1, arena);
-				case RIGHT:
-					return attack(player, Axis.X, 1, 1, arena);
+			Integer limit;
+			if (sideLength > 0) {
+				limit = 2;
+			} else {
+				limit = 1;
+			}
+			
+			for (Integer i = 0; i < limit; i++) {
+				Coordinate auxCoordinate = new Coordinate(targetCoordinate);
+				switch (i) {
+					case 1:
+						auxCoordinate = targetCoordinate.get(axis, frontModifier, Direction.LEFT);
+						break;
+					case 2:
+						auxCoordinate = targetCoordinate.get(axis, frontModifier, Direction.RIGHT);
+						break;
+				}
+				for (Integer sd = 0; sd <= sideLength; sd++) {
+					try {
+						auxCoordinate = auxCoordinate.get(axis, sd * frontModifier);
+						Entity entity = arena.getEntityFromCoordinate(auxCoordinate);
+						if (entity != null && entity.isAttackable()) {
+							Life target = (Life)entity;
+							
+							Weapon attackerWeapon = attacker.getWeapon();
+							target.damage(attackerWeapon.getDamage());
+							
+							List<String> messages = new ArrayList<String>();
+							
+							messages.add(String.format("{0} {1} suffered {2} damage from {3} {4}'s {5} {6}!", 
+										 new Object[]{target.getType(), target.getName(), String.valueOf(attackerWeapon.getDamage()),
+												 attacker.getType(), attacker.getName(), attackerWeapon.getType(), attackerWeapon.getName()}));
+							
+							if (target.isDead()) {
+								arena.removeEntityFromCoordinate(target, target.getPosition());
+								messages.add(String.format("{0} {1} is dead!", 
+									 	     new Object[]{target.getType(), target.getName()}));
+							} else if (target.isUnconscious() && target instanceof Player) {
+								messages.add(String.format("{0} {1} is unconscious!", 
+										 	 new Object[]{target.getType(), target.getName()}));
+							} else if (target.isUnconscious() && target instanceof Item) {
+								arena.removeEntityFromCoordinate(target, target.getPosition());
+								messages.add(String.format("{0} {1} is destroyed!", 
+									 	 new Object[]{target.getType(), target.getName()}));
+							}
+							
+							Queue.add(this);
+							
+							return new UI(String.join("\n", messages), true);
+						} else if (entity == null) {
+							System.out.println("Cell not found on Arena grid!");
+						} else if (!entity.isAttackable()) {
+							System.out.println("Entity is not attackable!");
+						}
+					} catch (Exception e) {
+						System.out.println(e.getMessage() + " - " + e.getStackTrace());
+					}
+				}
 			}
 		}
-		
-		return null;
-		
-	}
-	
-	static UI attack(Player attacker, Axis axis, Integer frontModifier, Integer sideLength, Arena arena) throws Exception {
-		
-//		Integer[][] attackDistance = attacker.getWeapon().getDistance();
-//		Integer frontDistance = attackDistance.length;
-//		Integer sideDistance = attackDistance[0].length;
-//		
-//		Coordinate attackerCoordinate = attacker.getPosition();
-		Coordinate targetCoordinate = attacker.getPosition().get(axis, frontModifier);
-		
-//		for (Integer fd = 0; fd < frontDistance; fd++) {
-//			for (Integer sd = 0; sd < sideDistance; sd++) {
-				try {
-					Entity entity = arena.getEntityFromCoordinate(targetCoordinate);
-					if (entity != null && entity.isAttackable()) {
-						Life target = (Life)entity;
-						
-						Weapon attackerWeapon = attacker.getWeapon();
-						target.damage(attackerWeapon.getDamage());
-						
-						List<String> messages = new ArrayList<String>();
-						
-						messages.add(String.format("{0} {1} suffered {2} damage from {3} {4}'s {5} {6}!", 
-									 new Object[]{target.getType(), target.getName(), String.valueOf(attackerWeapon.getDamage()),
-											 attacker.getType(), attacker.getName(), attackerWeapon.getType(), attackerWeapon.getName()}));
-						
-						if (target.isDead()) {
-							arena.removeEntityFromCoordinate(target, target.getPosition());
-							messages.add(String.format("{0} {1} is dead!", 
-								 	     new Object[]{target.getType(), target.getName()}));
-						} else if (target.isUnconscious() && target instanceof Player) {
-							messages.add(String.format("{0} {1} is unconscious!", 
-									 	 new Object[]{target.getType(), target.getName()}));
-						} else if (target.isUnconscious() && target instanceof Item) {
-							arena.removeEntityFromCoordinate(target, target.getPosition());
-							messages.add(String.format("{0} {1} is destroyed!", 
-								 	 new Object[]{target.getType(), target.getName()}));
-						}
-						
-						return new UI(String.join("\n", messages), true);
-					} else {
-						System.out.println("Cell not found on Arena grid or Entity is not attackable!");
-					}
-				} catch (Exception e) {
-					System.out.println(e.getMessage() + " - " + e.getStackTrace());
-				}
-//			}
-//		}
 				
 		return null;
 		
--- src/Utils/Coordinate.java
+++ src/Utils/Coordinate.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import Actions.Direction;
+
 public class Coordinate {
 	
 	private Integer x;
@@ -42,6 +44,62 @@ public class Coordinate {
 				return new Coordinate(this.x + value, this.y);
 			case Y:
 				return new Coordinate(this.x, this.y + value);
+		}
+		throw new Exception("No Axis to increase");
+	}
+	
+	public Coordinate get(Axis axis, Integer value, Direction direction) throws Exception {
+		switch (axis) {
+			case X:
+				switch (value) {
+					case 1:
+						switch (direction) {
+							case UP:
+								new Coordinate(this.x + 1, this.y);
+							case DOWN:
+								new Coordinate(this.x - 1, this.y);
+							case LEFT:
+								new Coordinate(this.x, this.y + 1);
+							case RIGHT:
+								new Coordinate(this.x, this.y - 1);
+						}
+					case -1:
+						switch (direction) {
+							case UP:
+								new Coordinate(this.x - 1, this.y);
+							case DOWN:
+								new Coordinate(this.x + 1, this.y);
+							case LEFT:
+								new Coordinate(this.x, this.y - 1);
+							case RIGHT:
+								new Coordinate(this.x, this.y + 1);
+						}
+				}
+			case Y:
+				switch (value) {
+					case 1:
+						switch (direction) {
+							case UP:
+								new Coordinate(this.x, this.y + 1);
+							case DOWN:
+								new Coordinate(this.x, this.y - 1);
+							case LEFT:
+								new Coordinate(this.x - 1, this.y);
+							case RIGHT:
+								new Coordinate(this.x + 1, this.y);
+						}
+					case -1:
+						switch (direction) {
+							case UP:
+								new Coordinate(this.x, this.y - 1);
+							case DOWN:
+								new Coordinate(this.x, this.y + 1);
+							case LEFT:
+								new Coordinate(this.x + 1, this.y);
+							case RIGHT:
+								new Coordinate(this.x - 1, this.y);
+						}
+				}
 		}
 		throw new Exception("No Axis to increase");
 	}
